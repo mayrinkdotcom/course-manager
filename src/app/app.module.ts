d1801e47862494b525a142c23e94ebbb518263f2
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-04-23 12:55:43 -0300

    feat: create pipes for course-list template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [ // local de declaração dos componentes que o módulo-raiz irá enxergar (direta ou indiretamente) para renderizar a aplicação
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
